d67455e74781bdc01d329ac2d7b9e649cfbdf9e6
Karol Hetman <karol.hetman@protonmail.com>
2023-09-09 13:43:30 +0100

    Loading .obj files

---
+++ src/main/java/com/commondrum/terrainrenderer/MeshLoader.java
@@ -0,0 +1,72 @@
+package com.commondrum.terrainrenderer;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.InputStream;
+
+import com.commondrum.terrainrenderer.Mesh;
+public class MeshLoader {
+    private ArrayList<float[]> vertices = new ArrayList<float[]>();
+    private ArrayList<int[]> faces = new ArrayList<int[]>();
+
+    public void load(String filename) {
+        BufferedReader reader;
+
+		try {
+			reader = new BufferedReader(new FileReader("src/main/resources/" + filename));
+			String line = reader.readLine();
+
+			while (line != null) {
+                if (line.startsWith("v ")) {
+                    vertices.add(parseVertex(line));
+                } else if (line.startsWith("f ")) {
+                    faces.add(parseFace(line));
+                }
+				line = reader.readLine();
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    private float[] parseVertex(String line) {
+        String[] parts = line.split(" ");
+        return new float[]{Float.parseFloat(parts[1]), Float.parseFloat(parts[2]), Float.parseFloat(parts[3])};
+    }
+
+    private int[] parseFace(String line) {
+        String[] parts = line.split(" ");
+        // get vertex index
+        String[] vertex1 = parts[1].split("/");
+        String[] vertex2 = parts[2].split("/");
+        String[] vertex3 = parts[3].split("/");
+        return new int[]{Integer.parseInt(vertex1[0]), Integer.parseInt(vertex2[0]), Integer.parseInt(vertex3[0])};
+    }
+
+    public ArrayList<float[]> getVertices() {
+        return vertices;
+    }
+
+    public ArrayList<int[]> getFaces() {
+        return faces;
+    }
+
+    public void print(){
+        for (float[] vertex : vertices) {
+            System.out.println("v " + vertex[0] + " " + vertex[1] + " " + vertex[2]);
+        }
+        for (int[] face : faces) {
+            System.out.println("f " + face[0] + " " + face[1] + " " + face[2]);
+        }
+    }
+
+    public static void main(String[] args) {
+        MeshLoader loader = new MeshLoader();
+        loader.load("cube.obj");
+        
+    }
+    
+}
