3216ac89e2293a74c87d32a9f3705fbc397fdb3e
Karol Hetman <karol.hetman@protonmail.com>
2023-09-09 14:36:25 +0100

    Cube rendered

--- src/main/java/com/commondrum/terrainrenderer/App.java
+++ src/main/java/com/commondrum/terrainrenderer/App.java
@@ -1,81 +1,104 @@
 package com.commondrum.terrainrenderer;
-
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.*;
 
-import javax.management.openmbean.OpenDataException;
-import javax.management.openmbean.OpenMBeanConstructorInfo;
+public class App {
 
-public class App 
-{
     private long window;
 
-    public void run() {
-        init();
-        loop();
-
-        // Free the window callbacks and destroy the window
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-
-        // Terminate GLFW and free the error callback
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
-    }
-
-    private void init() {
+    public void init() {
         if (!glfwInit()) {
-            throw new IllegalStateException("Unable to initialize GLFW");
+            throw new IllegalStateException("Failed to initialize GLFW");
         }
 
-        // Configure GLFW
-
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-        window = glfwCreateWindow(300, 300, "TerrainRenderer", NULL, NULL);
-
-        if(window == NULL) {
+        window = glfwCreateWindow(800, 600, "Simple Cube", 0, 0);
+        if (window == 0) {
             throw new RuntimeException("Failed to create the GLFW window");
         }
 
         glfwMakeContextCurrent(window);
-        glfwSwapInterval(1); // Enable v-sync
-        glfwShowWindow(window);
-
-    }
-
-    private void loop(){
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
         GL.createCapabilities();
-
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-        while(!glfwWindowShouldClose(window)) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+        glEnable(GL_DEPTH_TEST);
 
-            glfwSwapBuffers(window); // swap the color buffers
-
-            glfwPollEvents();
-        }
-
+        float aspectRatio = (float) 800 / 600;
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glFrustum(-aspectRatio, aspectRatio, -1, 1, 1.5f, 10.0f);
+        glMatrixMode(GL_MODELVIEW);
     }
 
+    public void loop() {
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+            glLoadIdentity();
+            glTranslatef(0.0f, 0.0f, -5.0f); 
+            glRotatef((float) (glfwGetTime() * 50), 1.0f, 1.0f, 0.0f);
 
+            drawCube();
 
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
 
+    private void drawCube() {
+        glBegin(GL_QUADS);
 
-    public static void main( String[] args )
-    {
-        new App().run();
+        // Front face
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+
+        // Back face
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+
+        // Left face
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+
+        // Right face
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+
+        // Top face
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+
+        // Bottom face
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+
+        glEnd();
+    }
+
+    public void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    public static void main(String[] args) {
+        App renderer = new App();
+        renderer.init();
+        renderer.loop();
+        renderer.cleanup();
     }
 }
--- src/main/java/com/commondrum/terrainrenderer/Mesh.java
+++ src/main/java/com/commondrum/terrainrenderer/Mesh.java
@@ -4,8 +4,20 @@ package com.commondrum.terrainrenderer;
 
 public class Mesh {
         private float[] vertices;
-        private float[] normals;
         private int[] indices;
+
+        public Mesh(float[] vertices, int[] indices) {
+            this.vertices = vertices;
+            this.indices = indices;
+        }
+
+        public float[] getVertices() {
+            return vertices;
+        }
+
+        public int[] getIndices() {
+            return indices;
+        }
 
 
     
